edaf2cbf2c88083d2f84748e015265ed63a76b42
Piyush Dhiman <83651098+PiyushD1712@users.noreply.github.com>
2024-07-11 16:57:24 +0530

    First Commit

--- app/src/main/java/com/example/rent_a_car/home/HomeActivity.java
+++ app/src/main/java/com/example/rent_a_car/home/HomeActivity.java
@@ -1,25 +1,44 @@
 package com.example.rent_a_car.home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 
 import com.example.rent_a_car.R;
 import com.example.rent_a_car.auth.viewmodel.AuthViewModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeActivity extends AppCompatActivity {
-    private Button btn;
     private AuthViewModel model;
+    private NavigationBarView navBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        btn = findViewById(R.id.idLogout);
         model = new ViewModelProvider(this).get(AuthViewModel.class);
-        btn.setOnClickListener(v->{
-            model.logoutUser();
+        navBar = findViewById(R.id.idBottomNavBar);
+        navBar.setOnItemSelectedListener( item-> {
+                int id = item.getItemId();
+                if(id==R.id.idHome){
+                    replaceFrag(new HomeFragment());
+                } else if (id==R.id.idPersonal) {
+                    replaceFrag(new PersonalFragment());
+                }
+                return true;
         });
+    }
+    private void replaceFrag(Fragment fragment){
+        FragmentManager fmg = getSupportFragmentManager();
+        FragmentTransaction frt = fmg.beginTransaction();
+        frt.replace(R.id.idHomeFrame,fragment);
+        frt.commit();
     }
 }
